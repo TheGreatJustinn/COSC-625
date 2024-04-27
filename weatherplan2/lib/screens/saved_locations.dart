130e87338f110931955cbed800fdd227f4559083
John Fontana <jfontana0@frostburg.edu>
2024-04-27 19:34:07 -0400

    refactoring saved locations with _buildheader() and added styling

--- weatherplan2/lib/screens/saved_locations.dart
+++ weatherplan2/lib/screens/saved_locations.dart
@@ -8,65 +8,96 @@ class SavedLocations extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      
       color: Colors.grey[850], // Dark background for the entire widget
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-            color: const Color.fromARGB(255, 67, 67, 67), // Slightly different background color for the header
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'Saved Locations',
-                  style: TextStyle(fontSize: 13.0, fontWeight: FontWeight.bold, color: Colors.white),
-                ),
-                ElevatedButton(
-                  onPressed: onSaveCurrentLocation,
-                  child: Text('Save Current Location'),
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Color(0xFF3C89EB), // Button background color
-                    foregroundColor: Colors.white, // Button text color
-                    elevation: 0, // No shadow for a flatter appearance
-                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
-                    padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                    textStyle: TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
-                  ),
-                ),
-              ],
-            ),
+          _buildHeader(context),
+          _buildLocationList(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildHeader(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [const Color.fromARGB(255, 67, 67, 67), Color.fromARGB(255, 48, 48, 48)],
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Color.fromARGB(255, 70, 141, 229),
+            offset: Offset(0, -10),
+            blurRadius: 15.0,
           ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: 5, // Number of saved locations
-              itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text(
-                    'Location ${index + 1}',
-                    style: TextStyle(
-                      fontSize: 16.0,
-                      fontWeight: FontWeight.normal,
-                      color: Colors.white, // White text color for visibility
-                    ),
-                  ),
-                  subtitle: Text(
-                    'temp, condition',
-                    style: TextStyle(
-                      fontSize: 14.0,
-                      fontWeight: FontWeight.normal,
-                      color: Colors.grey[400], // Light grey for subtitle
-                    ),
-                  ),
-                  onTap: () {
-                    // Action on tap
-                  },
-                );
-              },
+        ],
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            'Saved Locations',
+            style: TextStyle(fontSize: 13.0, fontWeight: FontWeight.bold, color: Colors.white),
+          ),
+          ElevatedButton(
+            onPressed: onSaveCurrentLocation,
+            child: Text('Save Current Location'),
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Color(0xFF3C89EB),
+              foregroundColor: Colors.white,
+              elevation: 0,
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+              textStyle: TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
             ),
           ),
         ],
+      ),
+    );
+  }
+
+  Widget _buildLocationList() {
+    return Expanded(
+      child: ShaderMask(
+        shaderCallback: (Rect bounds) {
+          return LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Colors.transparent, Colors.black, Colors.black, Colors.transparent],
+            stops: [0.0, .3, 0.95, 1.0],
+          ).createShader(bounds);
+        },
+        blendMode: BlendMode.dstIn,
+        child: ListView.builder(
+          itemCount: 5,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(
+                'Location ${index + 1}',
+                style: TextStyle(
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.normal,
+                  color: Colors.white,
+                ),
+              ),
+              subtitle: Text(
+                'temp, condition',
+                style: TextStyle(
+                  fontSize: 14.0,
+                  fontWeight: FontWeight.normal,
+                  color: Colors.grey[400],
+                ),
+              ),
+              onTap: () {
+                // Action on tap
+              },
+            );
+          },
+        ),
       ),
     );
   }
